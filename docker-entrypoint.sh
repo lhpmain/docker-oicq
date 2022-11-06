f78ab6e07b9fc7e9afca7794ae0208e467119937
lhpmain <lhpmain@126.com>
2022-11-07 00:02:28 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo -e "======================1. 启动OICQ========================\n"
-pm2 delete all
-pm2 start "oicq 315555943"
+#pm2 delete all
+#pm2 start "oicq 315555943"
 echo -e "OICQ启动成功...\n"
